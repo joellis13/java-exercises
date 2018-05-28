eac3fbb7284adb938b3345bc44235f2f668074cb
Jonathan Ellis <joellis13@yahoo.com>
2018-05-28 13:07:10 -0500

    Finished Class 2 Array for loop

---
+++ src/exercises/Class2/ArrayLoop.java
@@ -0,0 +1,12 @@
+package exercises.Class2;
+
+public class ArrayLoop {
+
+    public static void main(String[] args){
+        int[] nums = {1, 1, 2, 3, 5, 8};
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
