ecd96f213e87f8d7bb5586ed176f355093ae67db
Jonathan Ellis <joellis13@yahoo.com>
2018-05-28 11:31:24 -0500

    Finished Class 1 Exercises

---
+++ src/exercises/Class1/StringSearch.java
@@ -0,0 +1,20 @@
+package exercises.Class1;
+
+import java.util.Scanner;
+
+public class StringSearch {
+    public static void main(String[] args){
+        String alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+        String search;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("What substring would you like to search for?");
+        search = in.nextLine();
+
+        if (alice.toLowerCase().contains(search.toLowerCase())){
+            System.out.println("Yes, '" + search + "' is contained in the paragraph.");
+        } else {
+            System.out.println("No, '" + search + "' is not contained in the paragraph.");
+        }
+    }
+}
